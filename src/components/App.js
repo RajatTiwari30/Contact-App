e4175c0e99c112fec9deecc725f12e1ad12f2721
Rajat Tiwari <tiwari.rajat1098@gmail.com>
2022-02-25 03:06:45 +0530

    useState useEffect implementation

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,39 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import './App.css';
+import { v4 as uuid } from 'uuid';
 import Header from "./Header";
 import AddContact from "./AddContact";
 import ContactList from "./ContactList";
 
 function App() {
-  const contacts=[
-    {
-      id: "1",
-      name: "Rajat",
-      email: "tiwari.rajat1098@gmail.com"
-    },
-    {
-      id: "2",
-      name: "Tiwari",
-      email: "rajat.tiwari1098@gmail.com"
-    }
-  ]
+  const LOCAL_STORAGE_KEY = "contacts";
+  const [contacts,setContacts]= useState([])
+  const addContactHandler =(contact)=>{
+    setContacts([...contacts, { id: uuid(),...contact }]);
+  }
+
+  const removeContactHandler =(id)=>{
+    const newContactList = contacts.filter((contact) =>{
+      return contact.id !== id;
+    });
+
+    setContacts(newContactList);
+  }
+
+  useEffect(()=> {
+    const retrieveContacts=JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    if (retrieveContacts) setContacts(retrieveContacts);
+  }, [])
+
+  useEffect(()=> {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
+  }, [contacts])
+
   return (
     <div className="ui container">
       <Header />
-      <AddContact />
-      <ContactList contacts={contacts} />
+      <AddContact addContactHandler={addContactHandler} />
+      <ContactList contacts={contacts} getContactID={removeContactHandler}/>
     </div>
   );
 }
--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -2,9 +2,12 @@ import React from 'react';
 import ContactCard from './ContactCard';
 
 const ContactList= (props) =>{
+    const deleteContactHandler=(id)=>{
+        props.getContactID(id);
+    };
     const renderContact= props.contacts.map((contact) => {
         return(
-            <ContactCard contact={contact}></ContactCard>
+            <ContactCard contact={contact} clickHandler={deleteContactHandler} key={contact.id}></ContactCard>
         );
     });
     return(
